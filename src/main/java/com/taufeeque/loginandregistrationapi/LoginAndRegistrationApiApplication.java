2a185887bbb917c9ae5564e3f3db386f8396834f
Md Taufeeque Alam <md7870taufeequea@gmail.com>
2023-08-16 23:56:12 +0530

    Initial commit

---
+++ src/main/java/com/taufeeque/loginandregistrationapi/LoginAndRegistrationApiApplication.java
@@ -0,0 +1,13 @@
+package com.taufeeque.loginandregistrationapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoginAndRegistrationApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LoginAndRegistrationApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/taufeeque/loginandregistrationapi/LoginAndRegistrationApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taufeeque.loginandregistrationapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LoginAndRegistrationApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
